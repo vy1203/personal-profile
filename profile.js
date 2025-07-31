f2bd05a4592b529d22d9b2352f36c27d87f88fef
phanzyzy <139949957+vy1203@users.noreply.github.com>
2025-08-01 02:49:59 +0700

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,9 +1,8 @@
-// profile.js
 document.addEventListener('DOMContentLoaded', () => {
   const toggleButton = document.getElementById('themeToggle');
+  const downloadBtn = document.getElementById('downloadCV');
   const body = document.body;
 
-  // Kiểm tra trạng thái đã lưu
   const savedTheme = localStorage.getItem('theme');
   if (savedTheme === 'light') {
     body.classList.add('light-theme');
@@ -13,5 +12,18 @@ document.addEventListener('DOMContentLoaded', () => {
     body.classList.toggle('light-theme');
     const currentTheme = body.classList.contains('light-theme') ? 'light' : 'dark';
     localStorage.setItem('theme', currentTheme);
+  });
+
+  downloadBtn.addEventListener('click', () => {
+    const fileUrl = '.\profile.cv'; 
+    const fileName = 'cv.pdf'; 
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    console.log('Resume downloaded via JavaScript!');
   });
 });
